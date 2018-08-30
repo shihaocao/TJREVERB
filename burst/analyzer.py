edc27f49cb2619552f2afdcdf8fe0cc724507904
Shihao <shihaocao@gmail.com>
2018-08-30 18:58:56 -0400

    analyze done

--- burst/analyzer.py
+++ burst/analyzer.py
@@ -13,36 +13,48 @@ goalbytes = -1
 bperiod = -1
 datafails = 0
 for line in lines:
+    #print(line)
 
     if '_' in line:
+        print(len(line))
+        line = line[10:-1]
+        print(line)
         counter += 1
         if 'END' not in line:
             shorts += 1
+            print('shirt')
+            print(line)
             continue
+        if 'sat_py_beacon_bp_' not in line:
+            corrupts+=1
         if line[-1] == '.':
             line = line[:-1]
         if 'bp_' in line and '-' in line:
             bt = line[line.index('bp_')+3:line.index('-')]
             if bt is not bperiod:
-                print('datafail')
+                #print('datafail')
                 bperiod = bt
                 datafails += 1
         else:
             corrupts += 1
             continue
-        if 'bytes_' in line and ':' in line:
-            gt = line[line.index('bytes_')+1:line.index(':')]
+        if 'bytes=' in line and ':' in line:
+            gt = line[line.index('bytes=')+6:line.index(':')]
             if gt is not goalbytes:
                 goalbytes = gt
-                print('goalbytesfail')
+                #print('goalbytesfail')
                 datafails += 1
         else:
             corrupts += 1
             continue
         if '_bytes' in line:
             messagenum = line[line.index('-')+1:line.index('_bytes')]
+            messagenum = int(messagenum)
+            #print(counter)
+            #print(messagenum)
             if messagenum>counter:
                 losts += messagenum-counter
+                counter = messagenum
         else:
             corrupts += 1
             continue
@@ -53,3 +65,4 @@ print('----------')
 print('CORRUPTS: '+str(corrupts))
 print('SHORTS: '+str(shorts))
 print('LOSTS: '+str(losts))
+print(50-corrupts-shorts-losts)
