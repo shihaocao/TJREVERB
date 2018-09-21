7bcb5543474c76c039a0db217a25e44e506b0fa2
jjc8266 <jjc8266@gmail.com>
2018-09-21 19:44:37 +0000

    he

--- iridium-jacob/iridiumNotTest.py
+++ iridium-jacob/iridiumNotTest.py
@@ -1,6 +1,7 @@
-import sys
+
 import serial
 import time
+import sys
 
 debug = True
 
@@ -9,13 +10,13 @@ global ser
 def sendCommand(cmd):
     if cmd[-1] != '\r\n':
         cmd += '\r\n'
-    if debug:
-        print("Sending command: {}".format(cmd))
+    #if debug:
+        # # # print("Sending command: {}".format(cmd))
     ser.write(cmd.encode('UTF-8'))
     ser.flush()
     cmd_echo = ser.readline()
-    if debug:
-        print("Echoed: " + cmd_echo.decode('UTF-8'))
+    #if debug:
+        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # print("Echoed: " + cmd_echo.decode('UTF-8'))
 def setup(port):
     global ser
     ser = serial.Serial(port=port, baudrate = 19200, timeout = 15)
@@ -26,9 +27,9 @@ def doTheOK():
     sendCommand("AT")
     ser.readline().decode('UTF-8') # get the empty line
     resp = ser.readline().decode('UTF-8')
-    print (resp)
+    # # print (resp)
     if 'OK' not in resp:
-        print("Echo"+resp)
+        # # print("Echo"+resp)
         exit(-1)
 
     # show signal quality
@@ -37,13 +38,13 @@ def doTheOK():
     resp = ser.readline().decode('UTF-8')
     ser.readline().decode('UTF-8') # get the empty line
     ok = ser.readline().decode('UTF-8') # get the 'OK'
-    # print("resp: {}".format(repr(resp)))
+    # # # print("resp: {}".format(repr(resp)))
     if 'OK' not in ok:
-        print('Unexpected "OK" response: ' + ok)
+        # # print('Unexpected "OK" response: ' + ok)
         exit(-1)
     sendCommand("AT+SBDMTA=0")
-    if debug:
-        print("Signal quality 0-5: " + resp)
+    #if debug:
+        # # print("Signal quality 0-5: " + resp)
     ser.write("AT+SBDREG? \r\n".encode('UTF-8'))
     while True:
         try:
@@ -52,11 +53,8 @@ def doTheOK():
         except:
             continue
         break
-    if regStat == 2:
-        print("Already Registered")
-    else:
-        print("Not Registered, registering now...")
-        sendCommand("AT+SBDREG")
+    if regStat != 2:
+         sendCommand("AT+SBDREG")
     
 def main():
     argument = " "
@@ -68,25 +66,25 @@ def main():
         port = '/dev/ttyUSB0'
     setup(port)
     if len(sys.argv) < 4:
-        print("not enought args")
+        # # print("not enought args")
         exit(-1)
     else:
         if sys.argv[2] == "message":
             command = sys.argv[3]
-            print("Message to send: "+command)
+            # # print("Message to send: "+command)
         elif sys.argv[2] == "command":
             argument = sys.argv[3]
-            print("Command to execute: "+argument)
+            # # print("Command to execute: "+argument)
         elif sys.argv[2] == "listen":
-            print("Listening for Ring")
+            # # print("Listening for Ring")
             listenUp()
         else:
-            print("argument 3 is not valid, say either command, message or listen")
+            # # print("argument 3 is not valid, say either command, message or listen")
             exit(-1)
 
     #setup(port)
-    if debug:
-        print("Connected to {}".format(ser.name))
+    #if debug:
+        # # print("Connected to {}".format(ser.name))
 
     # clear everything in buffer
     #ser.reset_input_buffer()
@@ -96,11 +94,11 @@ def main():
 
     
     if ' ' not in argument:
-        print("Sending command: "+argument)
+        # # print("Sending command: "+argument)
         sendCommand(argument)
         exit(-1)
     if ' ' not in command:
-        print('Sending Message: '+command)
+        # # print('Sending Message: '+command)
         send(command)
 def listenUp():
     sendCommand("AT+SBDMTA=1")
@@ -110,7 +108,7 @@ def listenUp():
     iteration = 0
     while ringSetup != 2 :
         ring = ser.readline().decode('UTF-8')
-        print(ring)
+        # # print(ring)
         if "SBDRING" in ring:
             bytesLeft=1
             ser.timeout=120
@@ -119,37 +117,37 @@ def listenUp():
                 resp = "A"
                 while len(resp) < 2:
                     test = ser.readline().decode('UTF-8')
-                    #print("Response before Splitting: "+test+"tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt")
+                    # # #print("Response before Splitting: "+test+"tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt")
                     resp = test.split(': ')
-                #print("Response after splitting:  "+resp[1]+" 0 "+resp[0]+" END")
+                # # #print("Response after splitting:  "+resp[1]+" 0 "+resp[0]+" END")
                 try:
                     resp = resp[1].split(', ')
                 except:
-                    print("index out of bounds exception \r\n closing program")
+                    # # print("index out of bounds exception \r\n closing program")
                     exit(-1)
                 bytesLeft= int(resp[0])
-                #print("split response: "+resp[1])
+                # # #print("split response: "+resp[1])
                 #bytesLeft = 0
             sendCommand("AT+SBDRT")
             #while True:
                 #try:
-                    #print(ser.readline().decode('UTF-8').split(":")[1])
+                    # # #print(ser.readline().decode('UTF-8').split(":")[1])
 
-                    #print("done")
+                    # # #print("done")
                     #break
                 #except:
                     #continue
             ringSetup = 0
-            print(ser.readline().decode('UTF-8'))
-            print(ser.readline().decode('UTF-8'))
-            print(ser.readline().decode('UTF-8'))
-            print(ser.readline().decode('UTF-8'))
-            print(ser.readline().decode('UTF-8'))
-            print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
+            # # print(ser.readline().decode('UTF-8'))
             sendCommand("at+sbdmta=0")
             break
         #ser.flush()
-        print("listening...")
+        # # print("listening...")
 
 def send(thingToSend):
     # try to send until it sends
@@ -162,7 +160,7 @@ def send(thingToSend):
 
         signal = ser.readline().decode('UTF-8')#empty line
         signal = ser.readline().decode('UTF-8')
-        print("last known signal strength: "+signal)
+        # # print("last known signal strength: "+signal)
         # prepare message
         sendCommand("AT+SBDWT=" + thingToSend)
         ok = ser.readline().decode('UTF-8') # get the 'OK'
@@ -179,36 +177,36 @@ def send(thingToSend):
         #signal = ser.readline().decode('UTF-8')#empty line
         #signal = ser.readline().decode('UTF-8')#empty line
         while len(resp) > 0 and len(resp) <= 2:
-            print(resp)
+            # # print(resp)
             resp = ser.readline().decode('UTF-8')    
             resp = resp.replace(",", " ").split(" ")
             curTime = time.time()
             if (curTime-startTime)>30:
-                print("time out moving on")
+                # # print("time out moving on")
                 break
         # get the rsp
         
           #  if debug:
-        #print("resp: {}"t )
+        # # #print("resp: {}"t )
         try:
-            print("*******************" + str(resp))
+            # # print("*******************" + str(resp))
             alert = int(resp[1])
-            print(alert)
+            # # print(alert)
         except:
-            print("********************exception thrown")
+            # # print("********************exception thrown")
             continue
 
         #if debug:
-            #print("alert: {}".format(alert))
+            # # #print("alert: {}".format(alert))
     exit(-1)
 
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print(len(sys.argv))
-        print("Usage: $0 <serial port> <command or message or listenUp> <text to send or command>")
+        # # print(len(sys.argv))
+        # # print("Usage: $0 <serial port> <command or message or listenUp> <text to send or command>")
         exit(-1)
     try:
-        print(len(sys.argv))
+        # # print(len(sys.argv))
         main()
     finally:
         # sendCommand('ATE1', has_resp=False)
