26b4166bece5f52e7608d36a0106b303d7b163ad
OrionJam <jay.chen135@gmail.com>
2018-09-21 21:26:59 -0400

    eps_interface finally running jason 9/21

--- eps_sim/eps_sim_interface.py
+++ eps_sim/eps_sim_interface.py
@@ -1,7 +1,9 @@
 import smbus
+import time
+from smbus2 import *
 
 address = 43
-b = smbus.SMBus(1)
+bus = smbus.SMBus(1)
 
 SW0 = 2
 Sw1 = 3
@@ -35,29 +37,34 @@ GET_EXPECTED_PDM_STATE = b"0x16"
 GET_BOARD_TEMP = b"0x17"
 RESET_NODE = b"0x80"
 
+
 def pin_on(PDM_val):
-    b.write_i2c_block_data(address, 0x12, PDM_val)
+    with SMBusWrapper(1) as bus:
+        PDM_val = [PDM_val]
+        bus.write_i2c_block_data(address, 0x12, PDM_val)
 
 def pin_off(PDM_val):
-    b.write_i2c_block_data(address, 0x13, PDM_val)
+    with SMBusWrapper(1) as bus:
+        PDM_val = [PDM_val]
+        bus.write_i2c_block_data(address, 0x13, PDM_val)
 
-def get_board_status():
-    return b.read_i2c_block_data(address, 0x01)
+#def get_board_status():
+#    return b.read_i2c_block_data(address, 0x01)
 
-def set_system_watchdog_timeout(timeout):
-    b.write_i2c_block_data(address, 0x06, timeout)
+#def set_system_watchdog_timeout(timeout):
+#    b.write_i2c_block_data(address, 0x06, timeout)
 
-def get_BCR1_volts():
-    b.write_i2c_block_data(address, 0x10, 0x00)
-    return b.read_byte(address)
+#def get_BCR1_volts():
+#    b.write_i2c_block_data(address, 0x10, 0x00)
+#    return b.read_byte(address)
 
-def get_BCR1_amps_A():
-    b.write_i2c_block_data(address, 0x10, 0x01)
-    return b.read_byte(address)
+#def get_BCR1_amps_A():
+#    b.write_i2c_block_data(address, 0x10, 0x01)
+#    return b.read_byte(address)
 
-def get_BCR1_amps_B():
-    b.write_i2c_block_data(address, 0x10, 0x02)
-    return b.read_byte(address)
+#def get_BCR1_amps_B():
+#    b.write_i2c_block_data(address, 0x10, 0x02)
+#    return b.read_byte(address)
 
 while True:
     pin_on(SW0)
